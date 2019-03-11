539c291d6416799221aa40acf1ca484b555b4379
ksknight <korvette_00Whotmail.com>
2019-03-11 10:11:51 -0600

    added two games

---
+++ assests/Javascript/script.js
@@ -0,0 +1,7 @@
+$(".img3").on("click", function() {
+    window.location = "https://ksknight85.github.io/TriviaGame/index.html"
+})
+
+$(".img1").on("click", function() {
+    window.location = "https://ksknight85.github.io/unit-4-game/index.html"
+})
